da37d19709cd390f3aec5c9e1c3d7a337d6f4307
Urooj Usman <57406541+UroojUsman@users.noreply.github.com>
2021-08-21 23:09:08 +0500

    'firstCommit'

---
+++ src/components/Shelf.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './../App.css';
+import Book from './Book';
+class Shelf extends React.Component{
+    
+    render(){
+      const booksInShelf= this.props.books;
+        return(
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {booksInShelf.map(book=>(
+                      <li key={book.id}>
+                      <Book 
+                      book={book}
+                      
+                       bookTitle={book.title}
+                       bookAuthor={book.authors}
+                       bookshelf={book.shelf}
+                      Moveto={this.props.onShelfChange}
+                       />
+                    </li>
+                    ))}
+                    
+                  </ol>
+                </div>
+              </div>
+
+        );
+    }
+}
+
+export default Shelf;
